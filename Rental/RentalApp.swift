478edf46382f92fc406716f571b869c4e9abe185
kenmachdata <ken@machdata.com>
2022-09-02 13:28:08 -0500

    Initial Commit

---
+++ Rental/RentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalApp.swift
+//  Rental
+//
+//  Created by Kenneth Chatham on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
